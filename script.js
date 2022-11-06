e7dcf0b105b25c52a67c5250e280f7912165364c
Nate <fritz.nathaniel97@gmail.com>
2022-11-06 18:30:38 -0500

    adds logic to JS file for displaying current day, and coloring time blocks

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  //declaring variables
+  var dayNow = dayjs().format("dddd, MMMM D");
+  var hourNow = dayjs().hour();
+  //identifying HTML elements
+  var dayEL = $("#currentDay");
+  var timeBlocks = $("div.container-lg").children();
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,16 +16,23 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Iterates for each timeBlock div to set the colors with class names. Since each class is the same we can hard set them with a for loop, that way we don't have to remove them later when the hour changes.
+  for(i = 0; i < timeBlocks.length; i++) {
+    if(timeBlocks[i].id > hourNow) {
+      timeBlocks[i].className = "row time-block future"
+    } else if (timeBlocks[i].id < hourNow) {
+      timeBlocks[i].className = "row time-block past"
+    } else {
+      timeBlocks[i].className = "row time-block current"
+    };
+  };
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // Adds current day of the week and date to top of page.
+  dayEL.textContent = dayNow;
 });
 
