5042516f546462abbbc2dc06c2683cb483da56c1
Nate <fritz.nathaniel97@gmail.com>
2022-11-06 20:16:20 -0500

    adds event listeners for save buttons and logic to print from local storage

--- script.js
+++ script.js
@@ -8,16 +8,15 @@ $(function () {
   //identifying HTML elements
   var dayEL = $("#currentDay");
   var timeBlocks = $("div.container-lg").children();
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // Iterates for each timeBlock div to set the colors with class names. Since each class is the same we can hard set them with a for loop, that way we don't have to remove them later when the hour changes.
+  var saveButtons = $('button').toArray();
+  
+  // Adds current day of the week and date to top of page.
+  dayEL.textContent = dayNow;
+  
+  //starts iterating through each time block
   for(i = 0; i < timeBlocks.length; i++) {
+
+    //sets classes for colors based on current hour
     if(timeBlocks[i].id > hourNow) {
       timeBlocks[i].className = "row time-block future"
     } else if (timeBlocks[i].id < hourNow) {
@@ -25,14 +24,17 @@ $(function () {
     } else {
       timeBlocks[i].className = "row time-block current"
     };
-  };
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // Adds current day of the week and date to top of page.
-  dayEL.textContent = dayNow;
+    //adds event listeners to each button that call the following function on click
+    saveButtons[i].addEventListener("click", function(event){
+      //adds the innerHTML of the <textarea> next to the button to local storage with the key of the id from the div the button is in
+      localStorage.setItem(event.target.parent().id, event.target.prev().innerHTML);
+    });
+
+    //loads the local storage text if it exists
+    if(localStorage) {
+      timeBlocks[i].children[1].innerHTML = localStorage.getItem(timeBlocks[i].id);
+    };
+
+  }; 
 });
-
